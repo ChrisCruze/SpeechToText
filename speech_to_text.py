91c7550f51f128544b738be5cef4d9c8033fdd4c
Chris Cruz <chriscruz@chriss-mbp.myfiosgateway.com>
2021-11-08 13:08:55 -0500

    latest with latest script

--- speech_to_text.py
+++ speech_to_text.py
@@ -6,16 +6,38 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import subprocess
 import logging 
-
+import sys 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.INFO,format=LOG_FORMAT,filemode="w")
 logger = logging.getLogger()
 
+#https://stackoverflow.com/questions/404744/determining-application-path-in-a-python-exe-generated-by-pyinstaller
+def path_get():
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+    logger.info('application_path: %s', str(application_path))
+    return application_path
+
+def path_combine(arg):
+    file_path = os.path.join(path_get(),arg)
+    logger.info('file_path: %s', str(file_path))
+    return file_path
+
+def glob_get(arg):
+    glob_path = os.path.join(path_get(),arg)
+    logger.info('glob_path: %s', str(glob_path))
+    glob_directories = glob.glob(glob_path)
+    logger.info('%s: %s', arg,str(glob_directories))
+    return glob_directories
 
 
 def wav_convert_from_mov(file_name):
-    sound = AudioSegment.from_file(file_name+".mov", "mov")
-    sound.export(file_name+".wav", format="wav")
+    from_file_path = path_combine(file_name+".mov")
+    sound = AudioSegment.from_file(from_file_path, "mov")
+    export_file_path = path_combine(file_name+".wav")
+    sound.export(export_file_path, format="wav")
     logger.info('Converted %s from MOV to WAV', str(file_name))
 
 def chunks_from_sound(file_name):
@@ -35,6 +57,7 @@ def check_make_folder(folder_name):
 
 def chunks_create_from_file_name(file_name):
     logger.info('Breaking into Chunks')
+    file_name = path_combine(file_name)
     chunks = chunks_from_sound(file_name)
     folder_name = file_name#"audio-chunks"
     check_make_folder(folder_name)
@@ -49,7 +72,7 @@ def extension_remove_directories(directories):
     return [ntpath.basename(directory).split('.')[0] if '.' in directory else directory.replace('/','') for directory in directories] 
 
 def file_check_extension(file_name,extension):
-    directories = glob.glob("*"+extension)
+    directories = glob_get("*"+extension) #glob.glob("*"+extension)
     directories = extension_remove_directories(directories)
     has_file = file_name in directories
     logger.info('%s has %s: %s', str(file_name),str(extension),str(has_file))
@@ -73,6 +96,7 @@ def get_large_audio_transcription(file_name):
 
     r = sr.Recognizer()
 
+    file_name = path_combine(file_name)
     folder_name = file_name#"audio-chunks"
 
     logger.info('Creating File and Iterating Through Chunks')
@@ -103,7 +127,19 @@ def convert_file(file_name):
     if not has_text:
         get_large_audio_transcription(file_name)
 
-convert_file('Tim Goff 110221')
+def wav_mov_files_unique_get():
+    wav_files = extension_remove_directories(glob_get("*.wav"))
+    mov_files = extension_remove_directories(glob_get("*.mov"))
+    return list(set(mov_files+wav_files))
+
+def mov_wav_files_process():
+    mov_wav_file_names = wav_mov_files_unique_get()
+    for file_name in mov_wav_file_names:
+        convert_file(file_name)
+
+mov_wav_files_process()
+#print (wav_mov_files_unique_get())
+#convert_file('Tim Goff 110221')
 
 
 
@@ -112,67 +148,3 @@ convert_file('Tim Goff 110221')
 
 
 
-
-
-# file_check_extension(file_name,'/')
-
-# file_check_extension('jarvis_intro','.txt')
-
-def files_to_convert_get(base,target):
-    mov_files = glob.glob("*"+base)
-    wav_files = glob.glob("*"+target)
-
-    mov_files = extension_remove_directories(mov_files)
-    wav_files = extension_remove_directories(wav_files)
-    logger.info('Identifying %s files: %s', str(base),str(mov_files))
-    logger.info('Identifying %s files: %s', str(target),str(wav_files))
-    mov_files = [mov_file for mov_file in mov_files if mov_file not in wav_files]
-    logger.info('Remaining files: %s', str(mov_files))
-    return mov_files
-
-def mov_files_to_convert_get():
-    return files_to_convert_get('.mov','.wav')
-
-def wav_files_to_convert_get():
-    return files_to_convert_get('.wav','.txt')
-
-def wav_files_to_convert_get_chunk():
-    return files_to_convert_get('.wav','/')
-
-
-def write_file(file_name,text):
-    path = file_name + '.txt'
-    file_object = open(path,"w+")
-    file_object.write(text)
-    file_object.close()
-
-
-
-
-
-
-
-def txt_convert_from_wav(file_name):
-    txt = get_large_audio_transcription(file_name)
-    print (txt)
-    #write_file(file_name,txt)
-    
-
-
-
-def run():
-    mov_files = mov_files_to_convert_get()
-    [wav_convert_from_mov(mov_file) for mov_file in mov_files]
-    wav_files = wav_files_to_convert_get()
-    chunk_files = wav_files_to_convert_get_chunk()
-    [chunks_create_from_file_name(chunk_file) for chunk_file in chunk_files]
-    [txt_convert_from_wav(wav_file) for wav_file in wav_files]
-
-
-
-
-# print (os.listdir('Tim Goff 1102211'))
-# print (glob.glob('Tim Goff 1102211'))
-#wav_files_to_convert_get_chunk()
-#print (glob.glob("*/"))
-#run()
